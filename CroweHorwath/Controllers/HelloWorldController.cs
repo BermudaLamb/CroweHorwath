a2faf445164a38ca98a5acab67332cd3bc52ec14
BermudaLamb <thomlamb@gmail.com>
2017-10-05 12:35:23 -0500

    Added a Hello World Controller

---
+++ CroweHorwath/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using CroweHorwath.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroweHorwath.Controllers
+{
+    public class HelloWorldController
+    {
+        public HelloWorld HelloWorld;
+        public HelloWorldController()
+        {
+            HelloWorld = new HelloWorld();
+        }
+
+        public HelloWorld GetHelloWorld()
+        {
+            return HelloWorld;
+        }
+    }
+}
