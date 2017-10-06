5a288b848c09b9e971f3e7c83c0bd13c576a81e6
BermudaLamb <thomlamb@gmail.com>
2017-10-05 21:33:24 -0500

    Added NLog support as Console ouput

--- CroweHorwath/Program.cs
+++ CroweHorwath/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CroweHorwath.Controllers;
+using NLog;
 using System;
 
 namespace CroweHorwath
@@ -8,8 +9,22 @@ namespace CroweHorwath
         static void Main(string[] args)
         {
             var howdyWorld = new HelloWorldAPIController();
-            Console.WriteLine(howdyWorld.GetHelloWorld("Crowe Horwath").Value);
+            MLogger.Info(howdyWorld.GetHelloWorld("Crowe Horwath").Value);
             Console.ReadKey();
         }
+
+        #region Logging
+        private static Logger _mLogger;
+        public static Logger MLogger
+        {
+            get
+            {
+                if (_mLogger == null) _mLogger = LogManager.GetCurrentClassLogger();
+                return _mLogger;
+            }
+            set { _mLogger = value; }
+        }
+        #endregion
+
     }
 }
